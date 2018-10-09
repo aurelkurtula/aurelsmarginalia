4cd3b86ab8839bfdab4f71a3ae91531ad899d748
aurelkurtula <akurtula@gmail.com>
2018-10-09 17:01:16 +0100

    Updates

---
+++ precache-manifest.47532e7ee87916bdf9b935aa12df2e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aurelsmarginalia/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "05d5e6755bf50bcc295e",
+    "url": "/aurelsmarginalia/static/js/runtime~main.05d5e675.js"
+  },
+  {
+    "revision": "bd4933daf875282e7e35",
+    "url": "/aurelsmarginalia/static/js/main.bd4933da.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/aurelsmarginalia/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "bd4933daf875282e7e35",
+    "url": "/aurelsmarginalia/static/css/main.bca65610.chunk.css"
+  },
+  {
+    "revision": "77ba90fd7c5b250f6cd8145ab2e1653f",
+    "url": "/aurelsmarginalia/index.html"
+  }
+];
